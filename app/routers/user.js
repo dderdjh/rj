10ef2e1506eb5bcdd4f0790237b5acda4977e8c0
jasonding <djhdder@aliyun.com>
2019-07-04 18:58:07 +0800

    add longin router

--- app/routers/user.js
+++ app/routers/user.js
@@ -5,7 +5,7 @@ var router=express.Router();
 //注册路由
 router.post('/reg',function(req,res){
   var obj=req.body;
-  //判读输入是否为空
+  //判读数据是否为空
   var i=400;
   for (key in obj){
 	i++;
@@ -21,7 +21,26 @@ router.post('/reg',function(req,res){
     res.send({code:200,msg:'reg suc'});
   });
 });
-
+//登录路由
+router.post('/login',function(req,res){
+  var uname=req.body.uname,upwd=req.body.upwd;
+  console.log(uname,upwd);
+  //判断数据是否为空
+  if(!uname){
+    res.send({code:401,msg:'uname required'});
+	return;
+  }
+  if(!upwd){
+    res.send({code:402,msg:'upwd required'});
+	return;
+  }
+  var sql='SELECT * FROM rj_user WHERE uname=? && upwd=?';
+  pool.query(sql,[uname,upwd],function(err,result){
+    if(err) throw err;
+    res.send(result);
+	
+  });
+});
 
 //导出路由
 module.exports=router;
